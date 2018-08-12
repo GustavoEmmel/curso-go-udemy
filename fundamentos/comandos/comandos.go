a14eb36a6584d6507d7d434169d666fca9da3257
Gustavo Reichelt Emmel <gremmel@gmail.com>
2018-08-12 11:15:23 -0300

    finalizando aula 9

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Outro programa em %s! 1.1 \n", "Go")
+}
---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,15 @@
+// Programas executaveis iniciam pelo pacote main
+package main
+
+/*
+os codigos em go sao organizados em pacotes
+e para usa-los eh necessario declarar um ou varios imports
+*/
+
+import "fmt"
+
+// A porta de entrada de um programa go eh a funcao main
+func main() {
+	fmt.Print("primeiro ")
+	fmt.Print("programa")
+}
